5678d678d46c57b2c09a59de84240ecf9808ab87
Pavel Politov <politov@meetsidekick.com>
2024-03-12 13:42:29 +0400

    Upload text & pdf files to chatbot

--- gllama.py
+++ gllama.py
@@ -6,6 +6,8 @@ import requests
 import sys
 import yaml
 
+from pypdf import PdfReader
+
 params = {
     'temperature': 0.2,
     'top_k': 40,
@@ -35,8 +37,12 @@ def tokenize(content):
     r = requests.post(tokenize_url, data=json.dumps({'content': content}))
     return r.json()
 
-def show_prompt(message, history):
+def add_text(message, history):
   return '', history + [[message, '']]
+
+def add_file(history, file):
+  history = history + [((file.name,), None)]
+  return history
 
 def fetch_content(prompt, opt_params=None):
   payload = params.copy()
@@ -75,16 +81,29 @@ def predict(history, format_name, sysprompt):
   print('params:', params)
 
   messages = sysprompt_templ.format(sysprompt)
-  messages += "\n".join(["\n".join([user_template.format(item[0]), bot_template.format(item[1])]) for item in history])
+  # messages += "\n".join(["\n".join([user_template.format(item[0]), bot_template.format(item[1])]) for item in history])
+  for item in history:
+    if type(item[0]) is tuple:
+      print('Processing a file:', item[0][0])
+      text = read_file(item[0][0])
+      print('Processing done.')
+      if text is not None:
+        log_tokens(text)
+        text = 'CONTEXT: """' + text + '"""\n'
+        messages += add_linebreak(user_template.format(text))
+    else:
+      messages += add_linebreak(user_template.format(item[0]))
+      messages += add_linebreak(bot_template.format(item[1]))
   messages = messages.rstrip(end_tag)
   messages = messages.rstrip()
-  messages += "\n"
+  messages = add_linebreak(messages)
 
   history[-1][1] = ''
   for line in fetch_content(messages):
     history[-1][1] += line
     yield history
 
+  save_history(history)
   return history
 
 def save_history(history):
@@ -107,9 +126,35 @@ def grammar_check(prompt):
   revised_prompt = ''.join(fetch_content(ss, params2))
   return prompt + revised_prompt
 
+def read_file(filename):
+  if filename.endswith('.txt'):
+    with open(filename) as f:
+      return ''.join(f.readlines())
+  elif filename.endswith('.pdf'):
+    reader = PdfReader(filename)
+    text = ''
+    for page in reader.pages:
+      text += page.extract_text()
+    return text
+  return None
+
+def add_linebreak(s):
+  return s + '\n' if not s.endswith('\n') else s
+
+def log_tokens(text):
+  tokens = tokenize(text)
+  if 'tokens' in tokens:
+    print('log_tokens:', len(tokens['tokens']))
+  else:
+    print('log_tokens: unknown')
+
+
 def main():
   CSS ="""
   #chatbot { min-height: 500px; }
+  #chatbot .message { padding: 24px; }
+  .compact-row { gap: 10px; padding: 0; }
+  .custom-button { min-width: 40px; max-width: 40px; border-radius: 5px; }
   """
 
   with gr.Blocks(css=CSS, title='LocalLlama',
@@ -119,23 +164,30 @@ def main():
                            layout='panel',
                            show_copy_button=True,
                            render_markdown=True,
-                           scale=1,
+                           bubble_full_width=False,
                            height=500,
                            container=False)
-    with gr.Row():
+    with gr.Row(elem_classes='compact-row'):
       msg = gr.Textbox(autofocus=True,
                        lines=2,
                        show_label=False,
                        container=False)
+      upload_btn = gr.UploadButton(label="📁", variant="secondary", size="sm",
+                                   interactive=True,
+                                   elem_classes="custom-button")
+      submit_btn = gr.Button(value="▶", variant="primary", size="sm",
+                             elem_classes="custom-button")
     with gr.Row():
-      with gr.Column(scale=1):
+      with gr.Column(scale=2):
         pass
       with gr.Column(scale=1):
         with gr.Row():
-          check_btn = gr.Button(value="Check", variant="secondary", size="sm")
-          clear = gr.Button(value="Clear", variant="secondary", size="sm")
-          stop = gr.Button(value="Stop", variant="stop", size="sm")
-          # submit = gr.Button(value="Send", variant="primary", size="sm")
+          with gr.Column(min_width=50, scale=1):
+            check_btn = gr.Button(value="Check", size="sm")
+          with gr.Column(min_width=50, scale=1):
+            clear_btn = gr.Button(value="Clear", size="sm")
+          with gr.Column(min_width=50, scale=1):
+            stop_btn = gr.Button(value="Stop", variant="stop", size="sm")
 
     with gr.Accordion("Settings", open=False):
       with gr.Row():
@@ -176,18 +228,28 @@ def main():
         system_prompt = gr.Textbox(label='System prompt', value=sysprompts[0],
                                    interactive=True)
 
-    #submit_click_event = submit.click(fn=show_prompt, inputs=[msg, chatbot], outputs=[msg, chatbot], queue=False)\
-        #    .then(fn=predict, inputs=chatbot, outputs=chatbot)
-    submit_event = msg.submit(fn=show_prompt, inputs=[msg, chatbot], outputs=[msg, chatbot], queue=False)\
-        .then(fn=update_params, inputs=[temp, top_p, top_k, predictions, penalty, min_p], queue=False)\
-        .then(fn=predict, inputs=[chatbot, format_dropdown, system_prompt], outputs=chatbot)\
-        .then(fn=save_history, inputs=[chatbot])
+    add_text_args = dict(
+        fn=add_text, inputs=[msg, chatbot], outputs=[msg, chatbot], queue=False
+    )
+    update_params_args = dict(
+        fn=update_params, inputs=[temp, top_p, top_k, predictions, penalty, min_p], queue=False
+    )
+    predict_args = dict(
+        fn=predict, inputs=[chatbot, format_dropdown, system_prompt], outputs=chatbot
+    )
 
-    stop.click(fn=None, inputs=None, outputs=None, cancels=[submit_event], queue=False)
-    clear.click(lambda: None, None, chatbot, queue=False)
+    submit_event1 = msg.submit(**add_text_args).then(**update_params_args)\
+        .then(**predict_args)
+    submit_event2 = submit_btn.click(**add_text_args)\
+        .then(**update_params_args).then(**predict_args)
+
+    stop_btn.click(fn=None, inputs=None, outputs=None,
+                   cancels=[submit_event1, submit_event2], queue=False)
+    clear_btn.click(lambda: None, None, chatbot, queue=False)
     prompts.select(lambda x: x, inputs=[prompts], outputs=system_prompt)
 
     check_btn.click(fn=grammar_check, inputs=[msg], outputs=[msg])
+    upload_btn.upload(add_file, inputs=[chatbot, upload_btn], outputs=chatbot)
 
   demo.queue()
   demo.launch()
